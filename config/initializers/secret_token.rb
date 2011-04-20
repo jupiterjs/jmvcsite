cfa9a0f71e784dc8d4d3c4c86d831c45c97ee2b2
retro <konjevic@gmail.com>
2011-04-20 09:46:12 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JmvcCommunity::Application.config.secret_token = '233752783092c4c73244b7a4c32b688d21c6ef0afaa9cbce3eaef4edd9502dc1ce6eb3a44df360e10e7df718c22aa16680ca06e502ae513d2d080511f4526fbf'
